8d67356a2796dc6688002ee97c778de22c910460
mtmarctoni <marctonimas@outlook.es>
2024-02-18 16:01:37 +0100

    quickCommit

--- main.js
+++ main.js
@@ -2,44 +2,28 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs').promises;
-require("dotenv").config();
+//require("dotenv").config();
 const download = require('download');
 const path = require("path");
-const nodemailer = require('nodemailer');
 const ora = require('ora');
 
+//import global variables
+const {
+    domainUrl,
+    url,
+    wordsToSearch,
+    customers,
+    emailUser,
+    emailBody,
+    months
+} = require('./modules/global')
 
-//Define constants
-// The URL of the BOIB website or the specific page you want to scrape
-const domainUrl = 'https://www.caib.es'
-const url = domainUrl.concat('/eboibfront/ca');
-//words to search in Boib descriptions
-const wordsToSearch = ["FOTOPAR", "Energia i Canvi Climàtic", "Transcició Energètica i Canvi CLimàtic", "FEDER"];
-//customers to look for
-const customers = ['John Doe', 'Marisa Geller']
-//email constants
-const emailUser = process.env.OUTLOOK_USER
-const emailPassword = process.env.OUTLOOK_PASSWORD
-const emailBody = 
-`Hola, este es un correo automático.
-        
-Adjunto estan los BOIBs encontrados según los siguientes criterios de búsqueda:
- 
-- ${wordsToSearch}
- 
-Que tengas un buend día.`
-let transporter = nodemailer.createTransport({
-    service: 'Outlook365',
-    auth: {
-        user: emailUser,
-        pass: emailPassword
-    }
-});
-//auxiliary constants
-const months = ['gener', 'febrer', 'març', 'abril', 'maig', 'juny', 'juliol', 'agost', 'setembre', 'octubre', 'novembre', 'desembre'];
-let lastBoibInfo = {};
-let previousBoibLink = "";
-let downloadedPdfPaths = [];
+let {
+    transporter,
+    lastBoibInfo,
+    previousBoibLink,
+    downloadedPdfPaths
+} = require('./modules/global')
 
 const resetInfo = () => {
     lastBoibInfo = {
@@ -52,6 +36,7 @@ const resetInfo = () => {
         sectionLinks: []
     } 
 }
+
 
 const readDataBase = () => {
 
---
+++ modules/global.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+//Define constants
+// The URL of the BOIB website or the specific page you want to scrape
+const domainUrl = 'https://www.caib.es'
+const url = domainUrl.concat('/eboibfront/ca');
+//words to search in Boib descriptions
+const wordsToSearch = [
+    "FOTOPAR",
+    "Energia i Canvi Climàtic",
+    "Transcició Energètica i Canvi CLimàtic",
+    "FEDER",
+    "Llucmajor"
+];
+
+//customers to look for
+const customers = ['John Doe', 'Marisa Geller']
+
+//email constants
+const emailUser = process.env.OUTLOOK_USER
+const emailPassword = process.env.OUTLOOK_PASSWORD
+const emailBody = 
+`Hola, este es un correo automático.
+
+Adjunto estan los BOIBs encontrados según los siguientes criterios de búsqueda:
+
+- ${wordsToSearch}
+
+Que tengas un buend día.`
+let transporter = nodemailer.createTransport({
+    service: 'Outlook365',
+    auth: {
+        user: emailUser,
+        pass: emailPassword
+    }
+});
+
+//auxiliary constants
+const months = ['gener', 'febrer', 'març', 'abril', 'maig', 'juny', 'juliol', 'agost', 'setembre', 'octubre', 'novembre', 'desembre'];
+let lastBoibInfo = {};
+let previousBoibLink = "";
+let downloadedPdfPaths = [];
+
+//export variables
+module.exports = {
+    domainUrl,
+    url,
+    wordsToSearch,
+    customers,
+    emailUser,
+    emailPassword,
+    emailBody,
+    transporter,
+    months,
+    lastBoibInfo,
+    previousBoibLink,
+    downloadedPdfPaths
+}
